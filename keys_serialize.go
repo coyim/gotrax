ba63dde20cd5a44335d006e3ee797f6e1fdfacb0
Ola Bini <ola@olabini.se>
2018-08-20 17:37:26 -0500

    Extract deserialization of values, not just serialization

--- keys_serialize.go
+++ keys_serialize.go
@@ -1,5 +1,7 @@
 package gotrax
 
+import "github.com/otrv4/ed448"
+
 func (p *PublicKey) Serialize() []byte {
 	keyType := []byte{0xBA, 0xD0}
 	switch p.keyType {
@@ -13,4 +15,55 @@ func (p *PublicKey) Serialize() []byte {
 
 func (s *EddsaSignature) Serialize() []byte {
 	return s.s[:]
+}
+
+func (s *EddsaSignature) Deserialize(buf []byte) ([]byte, bool) {
+	var ok bool
+	var res []byte
+	if buf, res, ok = ExtractFixedData(buf, 114); !ok {
+		return nil, false
+	}
+	copy(s.s[:], res)
+	return buf, true
+}
+
+func DeserializePoint(buf []byte) ([]byte, ed448.Point, bool) {
+	if len(buf) < 57 {
+		return buf, nil, false
+	}
+	tp := ed448.NewPointFromBytes([]byte{
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	})
+	tp.DSADecode(buf[0:57])
+	return buf[57:], tp, true
+}
+
+func (p *PublicKey) Deserialize(buf []byte) ([]byte, bool) {
+	var ok bool
+	pubKeyType := uint16(0)
+
+	if buf, pubKeyType, ok = ExtractShort(buf); !ok {
+		return nil, false
+	}
+
+	keyType := uint16(0xBAD0)
+	switch p.keyType {
+	case Ed448Key:
+		keyType = Ed448KeyTypeInt
+	case SharedPrekeyKey:
+		keyType = SharedPrekeyKeyTypeInt
+	}
+
+	if pubKeyType != keyType {
+		return nil, false
+	}
+
+	buf, p.k, ok = DeserializePoint(buf)
+	return buf, ok
 }
